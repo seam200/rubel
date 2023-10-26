1f64e2ab90a8086e9ffc92fe3c669fabf0dbba9d
Md Seam Hossain <110179532+seam200@users.noreply.github.com>
2023-10-26 12:38:33 +0600

    Add files via upload
    Signed-off-by: Md Seam Hossain <110179532+seam200@users.noreply.github.com>

---
+++ day-67/index.js
@@ -0,0 +1,76 @@
+// var myText = "amra javascript sikbo";
+// console.log(myText);
+
+//This is a single line comment//
+/* this is 
+a maltiline 
+comnent */
+
+/**
+ * dhaka
+ * is
+ * the
+ * catipal
+ * city
+ *
+ */
+
+// var amrName = "Rubel Hossen";
+// console.log(typeof amrName);
+
+// var stAge = 20;
+// console.log(typeof stAge);
+
+// var stStatus = fales;
+// console.log(typeof stStatus);
+
+// var stData = { name: "Rubel", age: 250, country: "bangladesh", single: true };
+// console.log(typeof stData);
+
+/**
+ * +
+ * -
+ * *
+ * /
+ * %
+ *
+ */
+
+/**
+ * =
+ * +=
+ * -=
+ * *=
+ * /=
+ * %=
+ *
+ */
+/**
+ * ==
+ * ===
+ * >
+ * <
+ * >=
+ * <=
+ * !=
+ * !==
+ *
+ */
+/**
+ * &&
+ * ||
+ *
+ */
+
+// var numberOne = 10;
+// numberOne += 5;
+// numberOne -= 7;
+// console.log(numberOne);
+
+// var rubel = 22;
+// var reju = 10;
+// console.log(rubel !== reju);
+
+var a = 20;
+var b = 50;
+console.log(a || b);
