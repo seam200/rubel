7e52910fe9596d69bc5fa548ce57589692edd6dc
Md Seam Hossain <110179532+seam200@users.noreply.github.com>
2023-10-28 12:20:13 +0600

    Add files via upload
    Signed-off-by: Md Seam Hossain <110179532+seam200@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,34 @@
+function myFunc(x) {
+  console.log(x);
+}
+
+// // myFunc("I am Rubel");
+// myFunc("Rubel  Hossen");
+// function amrFunc(n1 = 10, n2 = 20) {
+//   console.log(n1 + n2);
+// }
+
+// amrFunc(5, 6);
+// amrFunc(40, 50);
+// amrFunc();
+
+// // Array
+// var stName = ["Raja", "Poja", "kala", "sada"];
+// console.log(stName[3] + "" + stName[2]);
+// var sp = 0;
+// var ep = 9999;
+// while (sp <= ep) {
+//   console.log(sp);
+//   ++sp;
+// }
+
+// var Arr = ["Rejone", "Fakibaz", "Pro player", "Rohinga"];
+// var suru = 0;
+// while (suru < 4) {
+//   console.log(Arr[suru]);
+//   suru++;
+// }
+
+for (var i = 5; i <= 100; i += 5) {
+  console.log(i);
+}
