2bc64d20be32531d2e2a6addbc31df0c115a12a8
Md Seam Hossain <110179532+seam200@users.noreply.github.com>
2023-10-31 12:24:46 +0600

    Add files via upload
    Signed-off-by: Md Seam Hossain <110179532+seam200@users.noreply.github.com>

---
+++ day-70/script.js
@@ -0,0 +1,14 @@
+document.getElementById("rejone").innerHTML += "<h1>number one fakibaz </h1>";
+document.querySelector("#rejone").innerHTML += "<h3> number one fakibaz </h3>";
+console.log(document.querySelector("#rejone"));
+console.log(document.getElementById("reone"));
+const rubel = document.getElementsByClassName("rubel");
+
+rubel[0].innerHTML = "rubel is ailsha";
+rubel[1].innerHTML = "rubel er nid nai";
+rubel[2].innerHTML = "rubel shot cut khuce";
+rubel[3].innerHTML = "rubel ojon kom";
+alert("Don't Alert Me");
+document.write("this is javascript text");
+console.log(document.querySelectorAll(".rubel"));
+console.log(document.getElementsByClassName(".rubel"));
